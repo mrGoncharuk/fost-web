27192b2b21077d44febddeaf31e524ddb0fa6230
mrGoncharuk <catmanboy1@gmail.com>
2020-10-20 22:10:35 +0300

    detected issue with try block view::execute

--- Cpp/fost-urlhandler/routing.cpp
+++ Cpp/fost-urlhandler/routing.cpp
@@ -10,10 +10,11 @@
 #include "fost-urlhandler.hpp"
 #include <fost/urlhandler.hpp>
 #include <fost/log>
-
+#include <iostream>
 
 bool fostlib::urlhandler::service(fostlib::http::server::request &req) {
     // Before doing anything else run some sanity checks on the request
+    std::cout << "sanity check "<< std::endl;
     auto const rfsw = static_cast<std::string>(
             req.file_spec()); // TODO Should be string view
     if (rfsw[0] != '/' || rfsw.find("/..") != std::string::npos) {
@@ -41,19 +42,26 @@ bool fostlib::urlhandler::service(fostlib::http::server::request &req) {
                 "Fost-Request-ID",
                 fostlib::timestamp_nonce24b64u().underlying().underlying());
     }
-
+    std::cout << "parse config "<< std::endl;
     fostlib::json host_config = c_hosts.value();
+    std::cout << "parse done "<< std::endl;
     if (host_config.has_key(hostname)
         || host_config.has_key(fostlib::string())) {
         try {
             // Route the request to the right handler
+            std::cout << "try  "<< std::endl;
             auto view_conf = host_config
                     [host_config.has_key(hostname) ? hostname
                                                    : fostlib::string()];
+            std::cout << "view config "<< std::endl;
             auto path = coerce<string>(req.file_spec().underlying()).substr(1);
+            std::cout << "path  "<< std::endl;
             auto resource = view::execute(view_conf, path, req, host(hostname));
+            std::cout << "view execute done  "<< std::endl;
             req(*resource.first, resource.second);
+            std::cout << "try done "<< std::endl;
         } catch (fostlib::exceptions::exception &e) {
+            std::cout << "catch 1"<< std::endl;
             fostlib::log::error(c_fost_web_urlhandler)(
                     "",
                     "fostlib::urlhandler::service -- "
@@ -65,6 +73,7 @@ bool fostlib::urlhandler::service(fostlib::http::server::request &req) {
                     fostlib::mime::mime_headers(), L"text/html");
             req(response, 500);
         } catch (std::exception &e) {
+            std::cout << "catch 2"<< std::endl;
             fostlib::log::error(c_fost_web_urlhandler)(
                     "", "fostlib::urlhandler::service -- std::exception")(
                     "exception", "message",
@@ -76,13 +85,17 @@ bool fostlib::urlhandler::service(fostlib::http::server::request &req) {
             req(response, 500);
         }
     } else {
+        std::cout << "else "<< std::endl;
         fostlib::log::warning(c_fost_web_urlhandler)(
                 "", "fostlib::urlhandler::service -- No configured web site")(
                 "hostname", hostname)("config", host_config);
+        std::cout << "body "<< std::endl;
         fostlib::text_body response(
                 L"<html><body>No site found to service request</body></html>",
                 fostlib::mime::mime_headers(), L"text/html");
+        std::cout << "500 "<< std::endl;  
         req(response, 500);
     }
+    std::cout << "url handler done "<< std::endl;
     return true;
 }
